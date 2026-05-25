ead00a9ebea09750bffa016abc35fe634731ba11
Jianwei <hjianwei1@gmail.com>
2026-05-25 16:47:10 +0800

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.java
@@ -0,0 +1,32 @@
+// Time complexity: O(n), loop through each character in string of length n
+// Space complexity: O(m), m is the total number of unique chars in string
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int maxFrequency = 0;
+        int l = 0;
+        int res = 0;
+        HashMap<Character, Integer> counter = new HashMap<>();
+
+        for (int r = 0; r < s.length(); r++) {
+            char currChar = s.charAt(r);
+            // Add curr Character into hashmap
+            counter.put(currChar, counter.getOrDefault(currChar, 0) + 1);
+
+            // After adding curr char into hashmap, check if it is now the largest frequency character in window
+            maxFrequency = Math.max(maxFrequency, counter.get(currChar));
+
+            // Loop until window is valid (Valid if window_size - max_frequency <= k)
+            while (r - l + 1 - maxFrequency > k) {  // If > k, means more than k characters need to be replaced
+                // Update hashmap and shift left pointer
+                counter.put(s.charAt(l), counter.get(s.charAt(l)) - 1);
+                l++;
+            }
+
+            // Update result (Compare with VALID window size)
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+}
