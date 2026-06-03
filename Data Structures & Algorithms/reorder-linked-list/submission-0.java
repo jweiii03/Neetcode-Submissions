74ec58ba7845e2a74e7483030b677a80b6e8abeb
Jianwei <hjianwei1@gmail.com>
2026-06-03 15:09:25 +0800

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public void reorderList(ListNode head) {
+        // Idea: Find the middle of the linked list -> Split into two equal halves
+        // Then reverse the second half, and then reorder the list by alternating from both halves
+
+        // Find the middle of the linked list using fast and slow pointer 
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next; // End of this loop, slow will be pointing to middle of linked list
+        }
+
+        // Reverse second half of list, requires 3 ptrs
+        ListNode front = slow.next;
+        ListNode prev = slow.next = null;
+        while (front != null) {
+            ListNode temp = front.next;
+            front.next = prev;
+            prev = front;
+            front = temp;
+        }
+
+        // Now have two equal halves where head and prev ptr point to start of each list
+        // prev ptr points to second half of list since its reversed
+        ListNode first = head; // Pointer to start of first half
+        ListNode second = prev; // Pointer to second half
+        while (second != null && first != null) {
+            ListNode temp = first.next;
+            ListNode temp2 = second.next;
+            first.next = second;
+            second.next = temp;
+            second = temp2;
+            first = temp;
+        }
+    }
+}
