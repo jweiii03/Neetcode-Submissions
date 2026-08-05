851cfe9e981cfd8b38fd30150c0ae73947b3549a
Jianwei <hjianwei1@gmail.com>
2026-08-05 23:06:05 +0800

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> hs = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hs.size() > k) {
+                hs.remove(nums[i - k - 1]);
+            }
+
+            if (hs.contains(nums[i]))   return true;
+            hs.add(nums[i]);
+        }
+        return false;
+    }
+}
