e4aec79dd648f38867adbe648fb108d001e42f3b
Jianwei <hjianwei1@gmail.com>
2026-09-18 15:00:58 +0800

    Add: reorganize-string - submission-0

---
+++ Data Structures & Algorithms/reorganize-string/submission-0.java
@@ -0,0 +1,56 @@
+// Time complexity: O(nlogk), k = number of unique alphabets (Number of keys in hashmap), n = length of string
+// Since k < 26, runtime can be simplified to O(n)
+// Space complexity: O(k + n) = O(n)
+class Solution {
+    public String reorganizeString(String s) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            hm.put(c, hm.getOrDefault(c, 0) + 1);
+        }
+
+        PriorityQueue<Count> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b.counter, a.counter));
+
+        for (Map.Entry<Character, Integer> entry : hm.entrySet()) {
+            Count curr = new Count(entry.getValue(), entry.getKey());
+            maxHeap.offer(curr);
+        }
+
+        StringBuilder answer = new StringBuilder();
+        Count previous = null;
+
+        while (!maxHeap.isEmpty()) {
+            Count current = maxHeap.poll();
+
+            answer.append(current.c);
+            current.counter--;
+
+            // The previous character can now be used again because
+            // a different character was just appended.
+            if (previous != null && previous.counter > 0) {
+                maxHeap.offer(previous);
+            }
+
+            previous = current;
+        }
+
+        // Leftover occurrences mean no valid arrangement was possible.
+        if (previous != null && previous.counter > 0) {
+            return "";
+        }
+        
+        
+
+        return answer.toString();
+    }
+}
+
+class Count {
+    int counter;
+    char c;
+
+    public Count(int counter, char c) {
+        this.counter = counter;
+        this.c = c;
+    }
+}
